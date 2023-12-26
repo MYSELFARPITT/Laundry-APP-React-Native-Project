bed2c5d1a7e833b499a532df916140c673cee2fd
MYSELFARPITT <107039759+MYSELFARPITT@users.noreply.github.com>
2023-12-27 00:19:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+//import HomeScreen from "./screens/HomeScreen";
+import StackNavigator from "./StackNavigator";
+import store from "./store";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function App() {
+  return (
+    
+    <Provider store={store}>
+      <StackNavigator />
+      <StatusBar style="auto" />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
